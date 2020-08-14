ce7b401b4eb0b6c1ee1d570b4662ffb83e404203
Andrei Appelt <andrei.appelt@gmail.com>
2020-08-13 21:23:58 -0300

    initial commit

---
+++ vpag-server/src/main/java/br/com/vpag/vpagserver/VpagServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.vpag.vpagserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpagServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VpagServerApplication.class, args);
+	}
+
+}
---
+++ vpag-server/src/test/java/br/com/vpag/vpagserver/VpagServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vpag.vpagserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VpagServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
